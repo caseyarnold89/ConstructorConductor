7a9e6d9149fd15421ae80f8e8bc5209ccb6c20a1
Casey Arnold <caseyarnold89@gmail.com>
2016-01-23 10:43:41 -0700

    Spec cleared, but need to finish other parts of project

--- instanceArray.js
+++ instanceArray.js
@@ -3,6 +3,7 @@
     0) Tyler, tylermcginnis33@gmail.com, 'iLoveJS'
     1) Cahlan, cahlan@devmounta.in, 'iLoveHashtags'
     2) Lenny, lenny@theLenster.com, 'iLoveLentilSoup'
+    3) Casey, caseya@gmail.com, 'cool'
 */
 
 var User = function(name, email, pw){
@@ -13,17 +14,29 @@ var User = function(name, email, pw){
 
 //Create an Array called 'users' that will store all our instances of User.
 
-  //code here
+var users = [];
 
 
 //Now create and push into your users array 3 separate instances of User using the data from above in that exact order
 
-  //code here
+users.push(User('Tyler', 'tylermcginnis33@gmail.com', 'iLoveJS'));
+users.push(User('Cahlan', 'cahlan@devmounta.in', 'iLoveHashtags'));
+users.push(User('Lenny','lenny@theLenster.com','iLoveLentilSoup'));
 
 console.log('Tyler\'s information is ');
 //Console.log all of Tylers information
+var UserFind = function (arr, name, cb) {
+    var userName;
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === name) {
+            userName = name;
+        }
+    }
+    cb(userName);
+}
 
-  //code here
+var displayUserInfo = function (users, 
+}
 
 console.log('Lenny\'s information is ');
 //Now console.log all of Lennys information
@@ -33,9 +46,11 @@ console.log('Lenny\'s information is ');
 
 //Now create another instance of User using your own information and then add that to your users array.
 
-  //code here
+users.push(User('Casey','casey@gmail.com','cool'));
 
 console.log('All my users names are ');
 //Now loop through your users Array and console.log every users name. 
-
+for (var i = 0; i < users.length; i++) {
+    console.log('All my users names are ' + users[i]);
+}
   //code here
